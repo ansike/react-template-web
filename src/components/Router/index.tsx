ansike/react-template

feat: init template

src/components/Router/index.tsx
@@ -1,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "../../pages/Home";
+import Download from "../../pages/Download";
+
+export default function SelfRouter() {
+  return (
+    <Router basename="">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/download" element={<Download />} />
+      </Routes>
+    </Router>
+  );
+}

ansike/react-template-web

src/components/Router/index.tsx
@@ -1,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "../../pages/Home";
+import Download from "../../pages/Download";
+
+export default function AppRouter() {
+  return (
+    <Router basename="">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/download" element={<Download />} />
+      </Routes>
+    </Router>
+  );
+}